c3b7fbc77f744152d46f0d55cd99d2bf03ef522f
Ariel Biernacki <arielbiernacki@gmail.com>
2019-12-26 11:29:44 +0100

    Add connection to database

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const basicDebug = require('debug')('app:startup');
+const dbDebug = require('debug')('app:db');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const config = require('config');
 const home = require('./routes/home');
-// const mongoose = require('mongoose');
-// const morgan = require('morgan');
-// const config = require('config');
 
 const app = express();
+
+const connectionString = `mongodb://${config.get('db.user')}:${config.get('db.password')}@${config.get('db.address')}/${config.get('db.name')}`;
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => dbDebug('Connected to MongoDB...'))
+  .catch((err) => {
+    dbDebug('Could not connect to MongoDB.', err.message);
+  });
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  basicDebug('Morgan enabled...')
+}
 
 /*
 app.use(express.json());
@@ -18,4 +36,4 @@ app.use('/', home);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => basicDebug(`Listening on port ${port}`));
