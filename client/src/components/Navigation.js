0a1b17c7b7c8a6a914eb19a266edb9dd3724940f
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-12-27 12:52:16 +0100

    Add Navigation Component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Navigation.css'
 
-const Navigation = () => {
-  return (
-    <nav>
-      <div>
-        <Link to="/registration">Rejestracja</Link>
-        <Link to="/login">Logowanie</Link>
-      </div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/aboutUs">O nas</Link>
-        <Link to="/products">Sklep</Link>
-        <Link to="/services">Usługi</Link>
-        <Link to="/contact">Kontakt</Link>
-      </div>
-    </nav>
-  )
-}
+class Navigation extends React.Component {
+  state = {user: null};
+
+  renderPersonalizedNav() {
+    if (this.state.user === null) {
+      return (
+        <ul>
+          <li><Link to="/registration">Rejestracja</Link></li>
+          <li><Link to="/login">Logowanie</Link></li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul>
+          <li>Witaj <span>{this.state.user}</span></li>
+          <li><Link to="/cart" className="cart"><img src="/img/cart.png" alt="ikona koszyk" /></Link></li>
+          <li><Link to="/login">Wyloguj</Link></li>
+        </ul>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <Link className="logo" to="/">
+          <img src="/img/logoB.png" alt="logo ThinkTree" />
+        </Link>
+        <div className="nav_box">
+          {this.renderPersonalizedNav()}
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/aboutUs">O nas</Link></li>
+            <li><Link to="/products">Sklep</Link></li>
+            <li><Link to="/services">Usługi</Link></li>
+            <li><Link to="/contact">Kontakt</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  };
+};
 
 export default Navigation;
