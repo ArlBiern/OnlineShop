9e5935580d4297dbf8691f10e26dbe41ef953883
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-12-30 22:36:35 +0100

    Update style for Services

--- client/src/reducers/servicesReducer.js
+++ client/src/reducers/servicesReducer.js
@@ -2,22 +2,22 @@ import { GET_SERVICES } from '../actions/types';
 
 const servicesData = [
   {
-    title: 'Usługa 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    price: 500,
-    image: '/img/products/kolczyki_ludek.png'
+    title: 'Wykonanie szkiców, portretów, obrazów w dowolnej stylizacji',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    price: '200 - 1200zł (zależna od wielkości, czasochłonności oraz wybranej techniki)',
+    image: '/img/obrazy.png'
   },
   {
-    title: 'Usługa 2',
+    title: 'Przygotowanie przedmiotów według Twojego pomysłu',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    price: 300,
-    image: '/img/products/zyrandol_mozaika.png'
+    price: 'Zależna od wielkości przedmiotu, czasochłonności wykonania i materiałów',
+    image: '/img/personalizacja.png'
   },
   {
-    title: 'Usługa 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    price: 150,
-    image: '/img/products/poduszka_chmurka.png'
+    title: 'Wykonanie obrazów naściennych',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Se cillum dolore eu fugiat nulla pariatur.',
+    price: '150 - 400 zł/m² (zależna od skomplikowania wzoru)',
+    image: '/img/obrazy_nascienne.png'
   }
 ]
 
